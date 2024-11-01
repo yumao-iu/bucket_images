a5d13347d835b7fbadb24ea7ba847dc5e4604ac2
hl <1799498990@qq.com>
2024-11-01 19:47:27 +0800

    new

--- nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: {
-    enabled: false,
-  },
-  modules: ['nuxt-swiper','@pinia/nuxt','@pinia-plugin-persistedstate/nuxt'],
-})
